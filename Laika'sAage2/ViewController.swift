514d8f726cc3c39338a112021fc02c84750f96a0
Caseyyu2 <caseyyu2@gmail.com>
2015-01-17 15:36:26 -0800

    Initial Commit

---
+++ Laika'sAage2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Laika'sAage2
+//
+//  Created by Casey Yu on 1/17/15.
+//  Copyright (c) 2015 Casey Yu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
